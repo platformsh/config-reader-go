1434f217e470a18fb060e38e9cbbfb4c0283d7cb
chadcarlson <chad.carlson@platform.sh>
2019-07-03 14:57:07 -0400

    Add MongoDB formatter.

--- libpq/libpq.go
+++ libpq/libpq.go
@@ -5,7 +5,7 @@ import (
   psh "github.com/platformsh/config-reader-go/v2"
 )
 
-// lib/pq requires a specfic connection string to connect to PostgreSQL.=
+// lib/pq requires a specfic connection string to connect to PostgreSQL.
 func FormattedCredentials(creds psh.Credential) (string, error) {
   formatted := fmt.Sprintf("host=%s port=%d user=%s " + "password=%s dbname=%s sslmode=disable",
     creds.Host, creds.Port, creds.Username, creds.Password, creds.Path)
---
+++ mongo/mongo.go
@@ -0,0 +1,13 @@
+package mongo
+
+import (
+  "fmt"
+  psh "github.com/platformsh/config-reader-go/v2"
+)
+
+// The mongo-go-driver requires a specific string to connect to MongoDB.
+func FormattedCredentials(creds psh.Credential) (string, error) {
+  formatted := fmt.Sprintf("%s://%s:%s@%s:%d/%s",
+    creds.Scheme, creds.Username, creds.Password, creds.Host, creds.Port, creds.Path)
+  return formatted, nil
+}
