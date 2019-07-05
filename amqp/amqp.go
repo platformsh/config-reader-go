95721c1346a27d6487ea1288a77fba030e55b691
chadcarlson <chad.carlson@platform.sh>
2019-07-05 13:52:06 -0400

    Add RabbitMQ/AMQP formatter.

---
+++ amqp/amqp.go
@@ -0,0 +1,13 @@
+package sqldsn
+
+import (
+  "fmt"
+  psh "github.com/platformsh/config-reader-go/v2"
+)
+
+// AMQP requires a specfic connection string to connect to PostgreSQL.
+func FormattedCredentials(creds psh.Credential) (string, error) {
+  formatted := fmt.Sprintf("%s://%s:%s@%s:%d/", creds.Scheme, creds.Username,
+    creds.Password, creds.Host, creds.Port)
+  return formatted, nil
+}
---
+++ amqp/amqp_test.go
@@ -0,0 +1,21 @@
+package sqldsn_test
+
+import (
+	psh "github.com/platformsh/config-reader-go/v2"
+	helper "github.com/platformsh/config-reader-go/v2/testdata"
+	amqp "github.com/platformsh/config-reader-go/v2/amqp"
+	"testing"
+)
+
+func TestAMQPFormatterCalled(t *testing.T){
+	config, err := psh.NewRuntimeConfigReal(helper.RuntimeEnv(psh.EnvList{}), "PLATFORM_")
+	helper.Ok(t, err)
+
+	credentials, err := config.Credentials("rabbitmq")
+	helper.Ok(t, err)
+
+	formatted, err := amqp.FormattedCredentials(credentials)
+	helper.Ok(t, err)
+
+  helper.Equals(t, "amqp://guest:guest@rabbitmq.internal:5672/", formatted)
+}
