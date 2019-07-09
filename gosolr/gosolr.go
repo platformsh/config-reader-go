8c8694d2dde43fe07b0e5b323bda15182aeba485
chadcarlson <chad.carlson@platform.sh>
2019-07-09 13:20:49 -0400

    Add Solr formatter.

---
+++ gosolr/gosolr.go
@@ -0,0 +1,26 @@
+package gosolr
+
+import (
+  "fmt"
+  "strings"
+  psh "github.com/platformsh/config-reader-go/v2"
+)
+
+type SolrCredentials struct {
+  Url        string
+  Collection string
+}
+
+// go-solr requires two separate strings to connect to Solr, a url and a collection name.
+func FormattedCredentials(creds psh.Credential) (SolrCredentials, error) {
+
+  var formatted SolrCredentials
+
+  path := strings.SplitAfter(creds.Path, "/")
+
+  formatted.Url = fmt.Sprintf("%s:%d/%s", creds.Host, creds.Port, path[0])
+  formatted.Collection = path[1]
+
+  return formatted, nil
+
+}
---
+++ gosolr/gosolr_test.go
@@ -0,0 +1,22 @@
+package gosolr_test
+
+import (
+	psh "github.com/platformsh/config-reader-go/v2"
+	helper "github.com/platformsh/config-reader-go/v2/testdata"
+	gosolr "github.com/platformsh/config-reader-go/v2/gosolr"
+	"testing"
+)
+
+func TestGoSolrFormatterCalled(t *testing.T) {
+	config, err := psh.NewRuntimeConfigReal(helper.RuntimeEnv(psh.EnvList{}), "PLATFORM_")
+	helper.Ok(t, err)
+
+	credentials, err := config.Credentials("solr")
+	helper.Ok(t, err)
+
+	formatted, err := gosolr.FormattedCredentials(credentials)
+	helper.Ok(t, err)
+
+  helper.Equals(t, "solr.internal:8080/solr/", formatted.Url)
+  helper.Equals(t, "collection1", formatted.Collection)
+}
